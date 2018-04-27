b8c36d0b680a662ca6251f3c93d15e0c6071dedc
Dang Chien Sy <chiensy195@gmail.com>
2018-04-28 02:13:50 +0800

    Add image loader plugin to story

--- src/pages/story/story.module.ts
+++ src/pages/story/story.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { StoryPage } from './story';
+import { IonicImageLoader } from 'ionic-image-loader';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { StoryPage } from './story';
   ],
   imports: [
     IonicPageModule.forChild(StoryPage),
+    IonicImageLoader
   ],
 })
 export class StoryPageModule {}
