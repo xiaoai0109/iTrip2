ff09415da4055c6f631e6a5df2bdb59b7b37b23d
Peiyan <xiaoai0109@gmail.com>
2018-05-02 11:44:48 +0800

    change the slide background color

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,9 +1,16 @@
 export const FIREBASE_CONFIG = {
     // Team's firebase
-    apiKey: "AIzaSyAAJ3au8BtpSLE7TvtQK9h9PYb8xaj-yro",
-    authDomain: "sg4207-a8f60.firebaseapp.com",
-    databaseURL: "https://sg4207-a8f60.firebaseio.com",
-    projectId: "sg4207-a8f60",
-    storageBucket: "sg4207-a8f60.appspot.com",
-    messagingSenderId: "556976966320"
+    // apiKey: "AIzaSyAAJ3au8BtpSLE7TvtQK9h9PYb8xaj-yro",
+    // authDomain: "sg4207-a8f60.firebaseapp.com",
+    // databaseURL: "https://sg4207-a8f60.firebaseio.com",
+    // projectId: "sg4207-a8f60",
+    // storageBucket: "sg4207-a8f60.appspot.com",
+    // messagingSenderId: "556976966320"
+    // Peiyan's firebase
+    apiKey: "AIzaSyCE_e9h_FrC1_1eS9BGDV_0782aT-npnjY",
+    authDomain: "firetest201804.firebaseapp.com",
+    databaseURL: "https://firetest201804.firebaseio.com",
+    projectId: "firetest201804",
+    storageBucket: "firetest201804.appspot.com",
+    messagingSenderId: "381475199385"
   };
